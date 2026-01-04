555cd5b03bed479d650f8b7c9d45ee5466f16349
ibrahim halil SOYDAN <ibrahimhalilsoydan@gmail.com>
2026-01-04 06:16:16 +0300

    ejs files have been created

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 import express from 'express';
 import path from 'node:path';
+import ejs from 'ejs';
 
 const app = express();
+
+
+//TEMPLATE ENGINE
+app.set("view engine", "ejs"); // ejs klasör yapısında viwes klasörüne bakar  bu yüzden temp klasörünü views yapıyıyoruz .
+
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+
 
 //bizim yazdığımız MIDDLEWARES
 /* const myLogger =(req,res,next)=>{
@@ -9,16 +20,42 @@ const app = express();
     next();
 } */
 
-//MIDDLEWARES
-app.use(express.static('public'));
+
 
 //burada yukarıda yazdıgmız myLogger Middlewareyi çağırıyoruz
 //app.use(myLogger)
 
-app.get('/', (req, res) => {
-  //res.sendFile("/home/ibrahimsoydan/js-World/PCAT02/temp/index.html")
-  res.sendFile(path.resolve('temp', 'index.html'));
-});
+
+//ROUTES ögrenme aşaması
+/* app.get('/', (req, res) => {
+  //res.sendFile("/home/ibrahimsoydan/js-World/PCAT02/temp/index.html") altta yapılanın uzun hali ama sadecebenim makinada çalışır
+  //res.sendFile(path.resolve('temp', 'index.html'));  ejs yapısından sonra bunu aşagıdaki gibi yapıyoruz .yollar ve yapılar degişti  temp views oldu vs.
+  res.render('index')
+}); */
+
+//ROUTES
+
+app.get('/',(req,res)=>{
+
+    res.render('index')
+})
+
+app.get('/about',(req,res)=>{
+
+    res.render('about')
+})
+
+app.get('/add',(req,res)=>{
+
+    res.render('add')
+})
+
+app.get('/video-page',(req,res)=>{
+
+    res.render('video-page')
+})
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
